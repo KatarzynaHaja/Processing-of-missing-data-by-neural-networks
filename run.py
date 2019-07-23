4a7e2a0d39f916d83e607cba3339a323a0ca1a36
Katarzyna Haja <katarzyna.haja@ardigen.com>
2019-07-23 16:38:16 +0200

    run

---
+++ run.py
@@ -0,0 +1,9 @@
+from ae_mean_on_last_layer import main as main_last
+from ae_sample import main as main_sample
+from ae_different_loss import main as main_diff
+
+
+
+main_sample()
+main_last()
+main_diff()
