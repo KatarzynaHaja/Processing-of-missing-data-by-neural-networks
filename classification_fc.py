e27d50476e7b53c042ae6c52f17a7e85424c8532
kasia <kasia.haja@gmail.com>
2019-11-24 16:57:01 +0100

    classificator

--- classification_fc.py
+++ classification_fc.py
@@ -7,7 +7,6 @@ import numpy as np
 import sys
 import os
 import analitic_method
-from layer_builder import LayerBuilder
 
 from visualization import Visualizator
 
@@ -15,19 +14,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class ClassificationFCParams:
-    def __init__(self, method, dataset, num_sample=None, ):
+    def __init__(self, method, dataset, num_sample=None):
         initializer = tf.contrib.layers.variance_scaling_initializer()
         self.num_input = 784
-        self.num_hidden_1 = 256  # 1st layer num features
-        self.num_hidden_2 = 128  # 2nd layer num features (the latent dim)
+        self.num_hidden_1 = 512
+        self.num_hidden_2 = 256  # 1st layer num features
+        self.num_hidden_3 = 128  # 2nd layer num features (the latent dim)
+        self.num_hidden_4 = 64
         self.num_output = 10
+
+        self.n_distribution = 5
+
+        self.num_layers = 4
 
         self.method = method
         self.dataset = dataset
         self.num_sample = num_sample
 
-        self.layer_inputs = [self.num_input, self.num_hidden_1,self.num_hidden_2 ]
-        self.layer_outputs = [self.num_hidden_1, self.num_hidden_2, self.num_output]
+        self.layer_inputs = [self.num_input, self.num_hidden_1, self.num_hidden_2, self.num_hidden_3, self.num_hidden_4]
+        self.layer_outputs = [self.num_hidden_1, self.num_hidden_2, self.num_hidden_3, self.num_hidden_4,
+                              self.num_output]
+
+
+        self.weights = []
+        self.biases = []
+        for i in range(self.num_layers + 1):
+            tf.Variable(initializer([self.num_input, self.num_hidden_1]))
+            self.weights.append(tf.Variable(initializer([self.layer_inputs[i], self.layer_outputs[i]])))
+            self.biases.append(tf.Variable(tf.random_normal([self.layer_outputs[i]])))
 
 
 class ClassificationFC:
@@ -38,8 +52,6 @@ class ClassificationFC:
         self.data_imputed_train = data_imputed_train
         self.data_imputed_test = data_imputed_test
         self.params = params
-        self.layer_builder = LayerBuilder(self.params)
-        self.n_distribution = 5
         self.X = tf.placeholder("float", [None, self.params.num_input])
         self.labels = tf.placeholder("float", [None, 10])
         self.gamma = gamma
@@ -58,12 +70,12 @@ class ClassificationFC:
 
         if self.params.method != 'imputation':
             self.sampling = Sampling(num_sample=self.params.num_sample, params=self.params, x_miss=self.x_miss,
-                                     n_distribution=self.n_distribution,
+                                     n_distribution=self.params.n_distribution,
                                      method=self.params.method)
 
     def set_variables(self):
         if self.params.method != 'imputation':
-            gmm = GaussianMixture(n_components=self.n_distribution, covariance_type='diag').fit(self.data_imputed_train)
+            gmm = GaussianMixture(n_components=self.params.n_distribution, covariance_type='diag').fit(self.data_imputed_train)
             self.p = tf.Variable(initial_value=gmm.weights_.reshape((-1, 1)), dtype=tf.float32)
             self.means = tf.Variable(initial_value=gmm.means_, dtype=tf.float32)
             self.covs = tf.abs(tf.Variable(initial_value=gmm.covariances_, dtype=tf.float32))
@@ -80,32 +92,37 @@ class ClassificationFC:
 
     def model(self):
         if self.params.method != 'imputation':
-            layer_1 = self.layer_builder.build_layer(self.x_known, self.params.layer_inputs_encoder[0],
-                                                     self.params.layer_outputs_encoder[0], 'relu')
             if self.params.method == 'theirs':
                 layer_1 = analitic_method.nr(self.gamma, self.covs, self.p, self.params, self.means,
-                                             self.x_miss, self.x_known, self.layer_builder.weights_1_layer,
-                                             self.layer_builder.bias_1_layer)
+                                             self.x_miss, self.x_known, self.params.weights[0],
+                                             self.params.biases[0])
             else:
+                layer_1 = tf.nn.relu(
+                    tf.add(tf.matmul(self.x_known, self.params.weights[0]),
+                           self.params.biases[0]))
                 samples = self.sampling.generate_samples(self.p, self.x_miss, self.means, self.covs,
                                                          self.params.num_input,
                                                          self.gamma)
 
-                layer_1_miss = self.sampling.nr(samples, self.layer_builder.weights_1_layer,
-                                                self.layer_builder.bias_1_layer)
+                layer_1_miss = self.sampling.nr(samples, self.params.weights[0],
+                                                self.params.biases[0])
                 layer_1_miss = tf.reshape(layer_1_miss, shape=(self.size[0], self.params.num_hidden_1))
 
                 layer_1 = tf.concat((layer_1_miss, layer_1), axis=0)
 
         if self.params.method == 'imputation':
-            layer_1 = self.layer_builder.build_layer(self.X, self.params.layer_inputs_encoder[0],
-                                                     self.params.layer_outputs_encoder[0], 'relu')
+            layer_1 = tf.nn.relu(
+                    tf.add(tf.matmul(self.X, self.params.weights[0]),
+                           self.params.biases[0]))
 
         layer = layer_1
 
-        for i in range(2):
-            layer = self.layer_builder.build_layer(layer, self.params.layer_inputs_encoder[i + 1],
-                                                   self.params.layer_outputs_encoder[i + 1], 'sigmoid')
+        for i in range(self.params.num_layers-1):
+            layer = tf.nn.relu(
+                tf.add(tf.matmul(layer, self.params.weights[i+1]), self.params.biases[i+1]))
+
+        layer = tf.add(tf.matmul(layer, self.params.weights[-1]), self.params.biases[-1])
+
 
         if self.params.method != 'last_layer':
             return layer
@@ -189,11 +206,10 @@ class ClassificationFC:
                 labels = self.labels_test[i * 2: (i + 1) * 2, :]
 
                 accuracy, accuracy_op, test_loss = sess.run([acc, acc_op, loss],
-                                                    feed_dict={self.X: batch_x, self.labels: labels})
+                                                            feed_dict={self.X: batch_x, self.labels: labels})
 
             print("Accuracy:", accuracy_op, "Train loss:", test_loss)
         return accuracy_op
-
 
 
 def run_model():
@@ -212,13 +228,13 @@ def run_model():
     data_imputed_test = imp.transform(data_test).reshape(data_test.shape[0], 784)
 
     params = [
-              {'method': 'theirs', 'params': [{'num_sample': 1, 'epoch': 250, 'gamma': 0.0}]},
-              {'method': 'last_layer', 'params': [{'num_sample': 10, 'epoch': 250, 'gamma': 0.0},
-                                                  {'num_sample': 20, 'epoch': 250, 'gamma': 0.0},
-                                                  {'num_sample': 100, 'epoch': 150, 'gamma': 1.0}]},
-              {'method': 'imputation', 'params': [{'num_sample': 1, 'epoch': 250, 'gamma': 0.0}]}
+        {'method': 'theirs', 'params': [{'num_sample': 1, 'epoch': 250, 'gamma': 0.0}]},
+        {'method': 'last_layer', 'params': [{'num_sample': 10, 'epoch': 250, 'gamma': 0.0},
+                                            {'num_sample': 20, 'epoch': 250, 'gamma': 0.0},
+                                            {'num_sample': 100, 'epoch': 150, 'gamma': 1.0}]},
+        {'method': 'imputation', 'params': [{'num_sample': 1, 'epoch': 250, 'gamma': 0.0}]}
 
-              ]
+    ]
     f = open('loss_results_classification_fc', "a")
     for eleme in params:
         for param in eleme['params']:
@@ -231,7 +247,6 @@ def run_model():
                     + str(param['epoch']) + ',' + str(param['gamma']) + ',' + str(accuracy))
             f.write('\n')
     f.close()
-
 
 
 run_model()
